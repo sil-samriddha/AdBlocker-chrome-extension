ae16c80102458a5c77a6741d0722547cd2f2ce89
Samriddha Sil <samriddha.sil297@gmail.com>
2023-04-16 20:48:58 +0530

    upload

---
+++ domains.js
@@ -0,0 +1,18 @@
+var Domains = [
+    "*://*.lb.usemaxserver.de/*",
+    "*://*.tracking.klickthru.com/*",
+    "*://*.zmedia.com/*",
+    "*://*.zv1.november-lax.com/*",
+    "*://*.google.com/adsid*",
+    "*://*.franecki.net/*",
+    "*://*.zodo.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.exponential.com/*",
+    "*://*.adbrite.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/* "
+    ];
